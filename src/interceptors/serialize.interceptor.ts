f7deb010d872eb7018f0973b883b3f7d085ece8d
Douglas Townsend <dougymt@gmail.com>
2023-06-07 13:50:54 -0400

    data serialization unit

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,38 @@
+import {
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  UseInterceptors,
+} from '@nestjs/common';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
+
+interface ClassConstuctor {
+  new (...args: any[]): Record<string, unknown>;
+}
+
+export function Serialize(dto: ClassConstuctor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
+  intercept(
+    context: ExecutionContext,
+    handler: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    // Run before request is handled by the handler
+
+    return handler.handle().pipe(
+      map((data: any) => {
+        // Run someting before the response is sent
+        return plainToClass(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
